f6c15e4d321ae511c36187593a8a3326f311c7cf
StanPeter <stanislav.petr7@gmail.com>
2020-12-04 02:31:02 +0100

    temporary db try

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from dotenv import load_dotenv
+import csv
 
 app = Flask(__name__)
 
@@ -28,24 +29,41 @@ def about():
 
 @app.route("/contact")
 def contact(is_form="show"):
-
     if request.args:
         is_form = request.args["is_form"]
 
-    print(is_form, "is_form")
     return render_template("contact.html", is_form=is_form)
+
+
+def write_to_file(data):
+    with open("tempDatabase.txt", mode="a") as db:
+        email = data["email"]
+        subject = data["subject"]
+        text = data["text"]
+        file = db.write(f"\n{email},{subject},{text}")
+
+
+def write_to_csv(data):
+    with open("tempDatabase.csv", mode="a") as db:
+        email = data["email"]
+        subject = data["subject"]
+        text = data["text"]
+        csv_writer = csv.writer(
+            db, delimiter=",", lineterminator='\n', quotechar='"', quoting=csv.QUOTE_MINIMAL
+        )
+        csv_writer.writerow([email, subject, text])
+    pass
 
 
 @app.route("/submit_form", methods=["POST", "GET"])
 def submit_form():
-    error = None
     if request.method == "POST":
         data = request.form.to_dict()
+        write_to_file(data)
+        write_to_csv(data)
 
-        print(data, "dataDDDDDDDDDDDDDDDD")
         return redirect(url_for("contact", is_form="hide"))
-    else:
-        return redirect("/")
+    return redirect("/")
 
 
 @app.errorhandler(404)
